dbd7d4abcb1c5fd3dd81abadb2035bf2459a4094
Marco Santonastasi <marco.santonastasi@gmail.com>
2021-06-01 12:11:06 +0200

    Booleans

--- 02_class-js_datatypes.js
+++ 02_class-js_datatypes.js
@@ -3,9 +3,69 @@ number,
 string,
 boolean,
 null,
-undefine 
+undefined
 */
 
-let myAwesomeInt = null;
+let ifCondition;
 
-console.log(typeof "myAwesomeInt");
+ifCondition = 1;
+
+if (ifCondition) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+/*
+let result;
+
+result = [1, 2] == [1, 2];
+console.log(result);
+console.log(typeof result);
+
+result = "" == "";
+console.log(result);
+console.log(typeof result);
+
+result = [1, 2] === [1, 2];
+console.log(result);
+console.log(typeof result);
+
+result = "" === "";
+console.log(result);
+console.log(typeof result);
+
+result = "1" === 1;
+console.log(result);
+console.log(typeof result);
+
+result = "1" !== 1;
+console.log(result);
+console.log(typeof result);
+
+result = "1" != 1;
+console.log(result);
+console.log(typeof result);
+
+result = "1" === "1";
+console.log(result);
+console.log(typeof result);
+
+result = 1 === 1;
+console.log(result);
+console.log(typeof result);
+
+result = 1 == 1;
+console.log(result);
+console.log(typeof result);
+
+result = "fsda2".localeCompare("asdfeA1");
+
+console.log(result);
+console.log(typeof result);
+
+result = "fsda2" > "asdfeA1";
+console.log(result);
+console.log(typeof result);
+
+*/
