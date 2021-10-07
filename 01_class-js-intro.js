ea28679c3b789721403fef4171aa6b18431d369c
dechilds1@gmail.com <dechilds1@gmail.com>
2021-10-07 09:21:04 +0200

    last push from mac

---
+++ 01_class-js-intro.js
@@ -0,0 +1,13 @@
+// This is a comment
+
+/* hghghghg
+
+hola this is the best class ever */
+
+console.log("Hello Pizza Bytes!");
+
+let cappucino = "Cappucino is good!";
+
+const daysInYear = 366;
+
+console.log(cappucino);
